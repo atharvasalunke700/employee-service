2636d2ea969c564709eee1e87f8cfffe82048bea
Atharva <atharvasalunke700@gmail.com>
2026-07-13 14:38:38 +0530

    Intial commit

---
+++ src/main/java/employeeservice/Controller/HomeController.java
@@ -0,0 +1,18 @@
+package employeeservice.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Employee Service!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Azure DevOps 🚀";
+    }
+}
